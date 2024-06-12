6045f25227bd6bd2e2b1b5e698b131873b2fe0b9
Jan Filip Sandvik <117898451+Blackadder96@users.noreply.github.com>
2024-06-12 09:25:52 +0200

    endret til mine egne bevegelse knapper og kommenter kode

--- slutt-prosjekt/scripts/player.gd
+++ slutt-prosjekt/scripts/player.gd
@@ -7,7 +7,11 @@ const JUMP_VELOCITY = -300.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+@onready var animated_sprite = $AnimatedSprite2D
 
+func _ready():
+	 # Koble signalet "frame_changed" fra animated_sprite til metoden _on_frame_changed i denne klassen
+	animated_sprite.connect("frame_changed", Callable(self,"_on_frame_changed"))
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -15,12 +19,12 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	# Få inngangsretningen og håndter bevegelsen/bremsingen.
+	# byttet til move left og move right i input map som gjør at man kan lage sine egne bevegelser knapper
+	var direction = Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
