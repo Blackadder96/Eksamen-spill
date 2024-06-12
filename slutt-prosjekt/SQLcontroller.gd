b3b586bd3dfc2d926a816c0de33cb7f384193338
Jan Filip Sandvik <117898451+Blackadder96@users.noreply.github.com>
2024-06-12 13:17:19 +0200

    database

--- slutt-prosjekt/SQLcontroller.gd
+++ slutt-prosjekt/SQLcontroller.gd
@@ -4,11 +4,23 @@ var database : SQLite
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	database = SQLite.new()
-	database.path"res://data.db"
+	database.path="res://data.db"
 	database.open_db()
 	pass # Replace with function body.
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_button_button_down():
+	var table =  {
+		"id" : {"data_type": "int" "primary_key" : true,"not_null":true, "auo_increment" : true
+		}
+	}
+
+
+func _on_button_2_button_down():
+	pass # Replace with function body.
